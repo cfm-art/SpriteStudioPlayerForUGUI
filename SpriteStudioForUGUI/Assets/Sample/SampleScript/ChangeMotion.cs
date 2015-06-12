bc8f924305348bc0f0022a809f4f60d78ebf2a47
art <none>
2015-06-12 16:55:30 +0900

    サンプルシーンをとりあえず用意。 TODO: その内サンプルデータも用意したい。

---
+++ SpriteStudioForUGUI/Assets/Sample/SampleScript/ChangeMotion.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using a.spritestudio;
+
+namespace a.smaple
+{
+    /// <summary>
+    /// モーション変更のサンプル
+    /// SpriteRoot#ChangeMotionを利用してモーションを変更する
+    /// </summary>
+    public class ChangeMotion
+        : MonoBehaviour
+    {
+        /// <summary>
+        /// シーンのヒエラルキー内に追加されているSSのデータ
+        /// </summary>
+        [SerializeField]
+        private SpriteRoot skelton_;
+
+        /// <summary>
+        /// モーション1
+        /// </summary>
+        [SerializeField]
+        private KeyFrameResource motion1_;
+
+        /// <summary>
+        /// モーション1
+        /// </summary>
+        [SerializeField]
+        private KeyFrameResource motion2_;
+
+        /// <summary>
+        /// インスペクターでちゃんと設定されているかチェック
+        /// </summary>
+        void Start()
+        {
+            if ( skelton_ == null ) {
+                Debug.LogError( "SSのデータ(skelton)が設定されていない" );
+            }
+            if ( motion1_ == null ) {
+                Debug.LogError( "モーションデータ(motion1)が設定されていない" );
+            }
+            if ( motion2_ == null ) {
+                Debug.LogError( "モーションデータ(motion2)が設定されていない" );
+            }
+        }
+
+        /// <summary>
+        /// GUI
+        /// </summary>
+        void OnGUI()
+        {
+            // ボタン押下でモーション変更
+            if ( GUILayout.Button( "モーション1", GUILayout.Width( 100 ), GUILayout.Height( 100 ) ) ) {
+                skelton_.ChangeMotion( motion1_ );
+            }
+
+            if ( GUILayout.Button( "モーション2", GUILayout.Width( 100 ), GUILayout.Height( 100 ) ) ) {
+                skelton_.ChangeMotion( motion2_ );
+            }
+        }
+    }
+
+}
---
+++ SpriteStudioForUGUI/Assets/Sample/SampleScript/Dynamic.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using a.spritestudio;
+
+namespace a.smaple
+{
+    /// <summary>
+    /// 動的にモーションを作る
+    /// </summary>
+    public class Dynamic
+        : MonoBehaviour
+    {
+        /// <summary>
+        /// 召喚する親ゲームオブジェクト
+        /// </summary>
+        [SerializeField]
+        private GameObject parent_;
+
+        /// <summary>
+        /// モーション
+        /// </summary>
+        [SerializeField]
+        private GameObject prefab_;
+
+        /// <summary>
+        /// インスペクターでちゃんと設定されているかチェック
+        /// </summary>
+        void Start()
+        {
+            if ( parent_ == null ) {
+                Debug.LogError( "プレハブをクローンする先になるゲームオブジェクト(parent)が設定されていない" );
+            }
+            if ( prefab_ == null ) {
+                Debug.LogError( "モーションデータのプレハブ(prefab)が設定されていない" );
+            }
+        }
+
+
+        /// <summary>
+        /// ボタン表示
+        /// </summary>
+        void OnGUI()
+        {
+            // ボタン押下でモーション再生
+            if ( GUILayout.Button( "召喚", GUILayout.Width( 100 ), GUILayout.Height( 100 ) ) ) {
+                // prefabをクローン
+                GameObject o = (GameObject) Object.Instantiate( prefab_ );
+                var root = o.GetComponent<SpriteRoot>();
+
+                // 親を指定
+                root.transform.SetParent( parent_.transform );
+
+                // 位置と大きさを適当に指定
+                root.SetPosition( Random.Range( -320, 320 ), Random.Range( -240, 240 - 100 ) );
+                root.SetScale( 0.3f, 0.3f );
+
+                // 完了時にゲームオブジェクトを削除
+                root.OnComplete += ( sprite ) => sprite.DestroySelf();
+            }
+        }
+    }
+}
